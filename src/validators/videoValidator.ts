fce01664946e40e4a2bc83456ef8a9b332b155b3
AHMAD <Ahmad.iznaurov@mail.ru>
2026-06-30 21:03:22 +0300

    new changing -new-

--- src/validators/videoValidator.ts
+++ src/validators/videoValidator.ts
@@ -97,13 +97,5 @@ export function validateUpdateVideo(data: any): ValidationError[] {
             message: 'Must be a boolean value.'
         });
     }
-
-    // --- MIN AGE RESTRICTION ---
-    if ((data.minAgeRestriction !== undefined && typeof data.minAgeRestriction === 'boolean') || data.minAgeRestriction < 1 || data.minAgeRestriction > 18) {
-        errors.push({
-            field: 'minAgeRestriction',
-            message: 'Must be a number.'
-        });
-    }
     return errors;
 }
