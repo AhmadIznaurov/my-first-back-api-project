7b8fc61bae069d0edfcaacacd5f4920f46c05a12
AHMAD <Ahmad.iznaurov@mail.ru>
2026-06-30 16:22:05 +0300

    new changing -4-00

--- src/models/videos.ts
+++ src/models/videos.ts
@@ -1,7 +1,7 @@
 export const VALID_RESOLUTIONS = ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"];
 
 export interface Video {
-    id: number | any;
+    id: number | string | any;
     title: string;
     author: string;
     canBeDownloaded: boolean;
