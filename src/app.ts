523660038ec2d4edf615cd45a29fce02845ad4f6
AHMAD <Ahmad.iznaurov@mail.ru>
2026-06-30 14:24:24 +0300

    new changing 3

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,9 @@ console.log(process.env.PORT)
 
 
 
-// app.use('/videos', videoControlRouter)
-// app.use('/videos/:id', videoControlRouter)
-// app.use('/testing/all-data', videoControlRouter)
+app.use('/videos', videoControlRouter)
+app.use('/videos/:id', videoControlRouter)
+app.use('/testing/all-data', videoControlRouter)
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
