0458f84b9f4f0242ae163c8c95538e39b97b252b
AHMAD <Ahmad.iznaurov@mail.ru>
2026-07-01 16:49:54 +0300

    new changing -champion-

--- src/validators/videoValidator.ts
+++ src/validators/videoValidator.ts
@@ -20,17 +20,11 @@ export function validateCreateVideo(data: any): ValidationError[] {
 
     // --- AUTHOR ---
 // Проверяем только если поле author было прислано в запросе
-    if (data.author !== undefined) {
-        if (typeof data.author !== 'string' || data.author.trim() === '') {
-            errors.push({ field: 'author', message: 'Author must be a non-empty string.' });
-        } else {
-            if (data.author.trim().length > 20) {
-                errors.push({
-                    field: 'author',
-                    message: 'Author must not exceed 20 characters.'
-                });
-            }
-        }
+    if (data.author.trim().length > 20) { // <-- УБЕДИТЕСЬ, ЧТО ЗДЕСЬ 20!
+        errors.push({
+            field: 'author',
+            message: 'Author must not exceed 20 characters.'
+        });
     }
     // --- AVAILABLE RESOLUTIONS ---
     if (!data.availableResolutions || !Array.isArray(data.availableResolutions) || data.availableResolutions.length === 0) {
